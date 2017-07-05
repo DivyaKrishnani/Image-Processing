b25851ac74a96d6a17a18d32390d357ddd8eaa34
Divya Krishnani <div.krish02@gmail.com>
2017-07-06 00:04:05 +0530

    Update imgrotate.py

--- imgrotate.py
+++ imgrotate.py
@@ -5,7 +5,6 @@ img = cv2.imread('2.jpg',0)
 rows,cols = img.shape
 
 M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
-#M = cv2.getRotationMatrix2D((cols/2,rows/2),180,1)
 dst = cv2.warpAffine(img,M,(cols,rows))
 
 cv2.imshow('img',dst)
