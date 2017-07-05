e1c96882af154a5c54bfaba12d8d0282fc1367c2
Divya Krishnani <div.krish02@gmail.com>
2017-07-06 00:03:39 +0530

    Add files via upload

---
+++ imgrotate.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('2.jpg',0)
+rows,cols = img.shape
+
+M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
+#M = cv2.getRotationMatrix2D((cols/2,rows/2),180,1)
+dst = cv2.warpAffine(img,M,(cols,rows))
+
+cv2.imshow('img',dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
