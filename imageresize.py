64fdefda4e3c13db837b74135a38ec464701129d
Divya Krishnani <div.krish02@gmail.com>
2017-07-06 00:01:13 +0530

    Update imageresize.py

--- imageresize.py
+++ imageresize.py
@@ -5,12 +5,10 @@ import numpy as np
 img= cv2.imread('2.jpg')
 
 """
-Preferable interpolation methods are cv2.INTER_AREA for shrinking and cv2.INTER_CUBIC (slow) & cv2.INTER_LINEAR for zooming. By default, interpolation method used is cv2.INTER_LINEAR for all resizing purposes.
+cv2.INTER_AREA for shrinking and cv2.INTER_CUBIC (slow) & cv2.INTER_LINEAR for zooming. By default, interpolation method used is cv2.INTER_LINEAR for all resizing purposes.
 """
 res = cv2.resize(img,None,fx=0.75, fy=0.75, interpolation = cv2.INTER_LINEAR)
 
-#height, width = img.shape[:2]
-#res = cv2.resize(img,(2*width,2*height), interpolation = cv2.INTER_AREA)
 
 cv2.imshow('IMAGE', res)
 
